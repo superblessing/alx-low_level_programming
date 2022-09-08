5da2bc14a0a4d0a0cf84de5d4d34afed7f5bd302
Superblessing <olanrewajusuperbless@gmail.com>
2022-09-08 13:09:08 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,7 @@
 
 
 
+
 /**
  * main - Prints a text
  *
@@ -10,6 +11,6 @@
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n")
 		return (0);
 }
