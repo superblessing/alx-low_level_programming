97076bb3b8d980b3f957f37ddef1cb981a00a422
Superblessing <olanrewajusuperbless@gmail.com>
2022-09-09 11:04:00 -0700

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,18 @@ int main(void)
 {
 	char a = 'a';
 
+	while (a <= 'z')
+	{
+		putchar (a);
+		a++;
+	}
+
+	a = 'A';
+
 	while (a <= 'Z')
 	{
-		putchar(a);
-		a++;
+	putchar (a);
+	a++;
 	}
 
 	putchar('\n');
