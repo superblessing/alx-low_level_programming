9c3c5765e2b509588e85b630259233e210e7879c
Superblessing <olanrewajusuperbless@gmail.com>
2022-09-27 06:45:33 -0700

    Task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,14 +8,12 @@
  * @b: value to set
  * Return: Always 0
  */
+
 char *_memset(char *s, char b, unsigned int n);
 {
-	int i = 0;
+	unsigned int i = 0;
 
-	for (; n > 0; i++)
-		{
-			s[i] = b;
-			n--;
-		}
+	for (i = 0; i < n; i++)
+		s[i] = b;
 	return (s);
 }
