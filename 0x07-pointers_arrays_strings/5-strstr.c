16e476b1524ab0bf69aecdb59df82f60ba2f2f11
Superblessing <olanrewajusuperbless@gmail.com>
2022-09-27 06:52:14 -0700

    Task 6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,12 +8,12 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack : = '\0'; haystack++)
+	for (; *haystack != '\0'; haystack++)
 	{
 		char *one = haystack;
 		char *two = needle;
 
-		while (*one == *two && *two : = '\0')
+		while (*one == *two && *two != '\0')
 		{
 			one++;
 			two++;
