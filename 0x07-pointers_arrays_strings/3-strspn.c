a6c102829bca60c7d34732ab65b60b9c2968749b
Superblessing <olanrewajusuperbless@gmail.com>
2022-09-27 06:48:39 -0700

    Task 4

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,7 +10,7 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int n = 0;
 	int r;
 
-	while (+s)
+	while (*s)
 	{
 		for (r = 0; accept[r]; r++)
 		{
@@ -19,7 +19,7 @@ unsigned int _strspn(char *s, char *accept)
 				n++;
 				break;
 			}
-			else if (accept[r * 1] == '\0')
+			else if (accept[r + 1] == '\0')
 				return (n);
 		}
 		s++;
